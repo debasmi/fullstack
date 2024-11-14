7bde473ac8b22c20764c54a06f8b22cfc78ba90f
DEBASMI_BASU <basudebasmi2006@gmail.com>
2024-11-14 20:40:45 +0530

    new change 3

--- workspace/littlelemon/little_lemon/urls.py
+++ workspace/littlelemon/little_lemon/urls.py
@@ -2,6 +2,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns=[
-    path('index/', views.index, name='index')
-]
+    path('index/', views.index, name='index'),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+   ]
--- workspace/littlelemon/littlelemon/urls.py
+++ workspace/littlelemon/littlelemon/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from little_lemon.views import BookingViewSet
+
+router = DefaultRouter()
+router.register('tables', BookingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('restaurant/',include('little_lemon.urls'))
+    path('restaurant/', include('little_lemon.urls')),
+    path('restaurant/booking/', include(router.urls)),
 ]
